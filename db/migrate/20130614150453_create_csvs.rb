3ea1139dd03c69134c0a50213e2369912cec24d1
Ryan Guerrettaz <rguerrettaz@yahoo.com>
2013-06-14 08:35:09 -0700

    Fix silly migration error
    Guess its been a while since I wrote a new migration file :/

--- db/migrate/20130614150453_create_csvs.rb
+++ db/migrate/20130614150453_create_csvs.rb
@@ -1,10 +1,10 @@
 class CreateCsvs < ActiveRecord::Migration
   def change
     create_table :csvs do |t|
-      t.og_filename   :string
-      t.og_url        :string
-      t.deduped_url   :string
-      t.deletions     :text
+      t.string :og_filename
+      t.string :og_url
+      t.string :deduped_url
+      t.text   :deletions
 
       t.timestamps
     end
